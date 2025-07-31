3b630d2240d617158af49897e7038d17e09e382d
Alban González <alban.gonzalez@outlook.com>
2025-07-31 13:33:25 -0500

    Enable style preview in Gutenberg editor

--- functions.php
+++ functions.php
@@ -18,4 +18,9 @@ function  achgo_enqueue_styles() {
 	);
 }
 
+function achgo_add_editor_styles() {
+	add_editor_style( get_stylesheet_uri() );
+}
+
 add_action( 'wp_enqueue_scripts', 'achgo_enqueue_styles' );
+add_action( 'after_setup_theme', 'achgo_add_editor_styles' );
