b8e461ba2223d08985ef779b753bc72f7bca36c1
Alban González <alban.gonzalez@outlook.com>
2025-08-03 13:15:51 -0500

    Post Header block

---
+++ build/blocks-manifest.php
@@ -0,0 +1,25 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'post-header' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'achgo/post-header',
+		'version' => '0.1.0',
+		'title' => 'Post Header',
+		'category' => 'theme',
+		'icon' => 'layout',
+		'description' => 'Post header showing title with a gradient background',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'textdomain' => 'post-header',
+		'editorScript' => 'file:./index.js',
+		'editorStyle' => 'file:./index.css',
+		'style' => 'file:./style-index.css',
+		'render' => 'file:./render.php'
+	)
+);
---
+++ build/post-header/render.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ */
+?>
+
+
+<div class="wp-block-achgo-post-header">
+    <div class="is-layout-constrained has-base-color">
+        <div class="content is-layout-flex alignwide">
+            <div class="heading">
+                <h1><?php the_title() ?></h1>
+            </div>
+        </div>
+    </div>
+</div>
